f028a1c060ecc1904181f43bf1d0d96c7bdb3e0a
malcovious <gilbert.spears@gmail.com>
2017-10-22 05:34:04 -0500

    fleshed out most of ripper

--- Ripper.py
+++ Ripper.py
@@ -1,12 +1,12 @@
 sudo yum install autoconf automake intltool pkgconfig glib2 gtk2 dbus-glib xfconf gio2.0 libglade2 perl libwnck* gudev-1.0
 
-
 IF(EndsWith([Coordinates Generated Id], “0”), Concat(Coordinates, “X”), Coordinates)
 
 import time
 import subprocess
 import select
 
+str toEnter
 f = subprocess.Popen([‘tail’,‘-F’, “*”, filename],\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 p = select.poll()
@@ -14,9 +14,10 @@ p.register(f.stdout)
 
 while True:
     if p.poll(1):
-        print f.stdout.readline()
-        #if line = headers, break
-        #divide line into an array using split
-        #see if key is already in the processed keys list (we make)
-        #if it isn’t, create the query and hit MSSQL
+        line = f.stdout.readline()
+        if "Domain" and "timeSpent" and "key" not in line:
+        # divide line into an array using split
+            toEnter = line.split(',')
+        # create the query and hit MSSQL
+            INSERT INTO [HowsMyProductivity].[dbo].[TestTable] VALUES (toEnter[0],int(toEnter[1]),int(toEnter[2]));
     time.sleep(1)
